6804c59e18bebbeaa405e3097dd94bbf5f81eddc
zhanghm1995 <zhanghm_1995@qq.com>
2022-11-18 17:46:12 +0800

    add files to support train in PL

---
+++ model_pl.py
@@ -0,0 +1,67 @@
+'''
+Copyright (c) 2022 by Haiming Zhang. All Rights Reserved.
+
+Author: Haiming Zhang
+Date: 2022-11-18 12:12:14
+Email: haimingzhang@link.cuhk.edu.cn
+Description: The Pytorch Lightning model.
+'''
+
+import torch
+import pytorch_lightning as pl
+
+from model import Network
+from decoder.utils.utils import *
+
+
+def weights_init_normal(m):
+    """ Weights initialization with normal distribution.. Xavier """
+    classname = m.__class__.__name__
+    if classname.find("Conv2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("Conv1d") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+    elif classname.find("BatchNorm1d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+
+
+class XMFNetPL(pl.LightningModule):
+    def __init__(self, config):
+
+        super().__init__()
+
+        self.model = Network().apply(weights_init_normal)
+        self.loss_cd = L1_ChamferLoss()
+
+        self.config = config
+
+    
+    def forward(self, partial, image):
+        partial = farthest_point_sample(partial, 2048)
+        partial = partial.permute(0, 2, 1)
+        complete = self.model(partial, image)
+        return complete
+
+    def training_step(self, batch, batch_idx):
+        image, gt, partial = batch
+        gt = farthest_point_sample(gt, 2048)
+
+        pred_complete = self(partial, image)
+        loss_total = self.loss_cd(pred_complete, gt)
+        self.log("train/loss", loss_total, prog_bar=True)
+        return loss_total
+
+    def validation_step(self, batch, batch_idx):
+        pass
+
+    def configure_optimizers(self):
+        optim_opt = self.config.optimizer
+        optimizer = torch.optim.Adam(filter(
+            lambda p: p.requires_grad, self.model.parameters()), 
+            lr=optim_opt.lr, betas=(0.9, 0.999))
+        return optimizer
+
---
+++ train_pl.py
@@ -0,0 +1,82 @@
+'''
+Copyright (c) 2022 by Haiming Zhang. All Rights Reserved.
+
+Author: Haiming Zhang
+Date: 2022-11-18 16:32:06
+Email: haimingzhang@link.cuhk.edu.cn
+Description: 
+'''
+
+import argparse
+import pytorch_lightning as pl
+import torch
+from torch.utils.data import DataLoader
+import time
+from omegaconf import OmegaConf
+
+from model_pl import XMFNetPL
+from dataloader import ViPCDataLoader, collate_fn
+
+
+def get_git_commit_id():
+    """Get the Git commit hash id for logging usage
+
+    Returns:
+        str: hash id
+    """
+    import git
+    repo = git.Repo(search_parent_directories=False)
+    sha = repo.head.object.hexsha
+    return sha
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', type=str, default='./cfg/official.yaml', help='the config file path')
+    parser.add_argument('--checkpoint', type=str, default=None, help="the pretrained checkpoint path")
+    parser.add_argument('--test_mode', action='store_true', help="whether is a test mode")
+
+    args = parser.parse_args()
+    config = OmegaConf.load(args.cfg)
+    config.update(vars(args)) # override the configuration using the value in args
+
+    time_str = time.strftime("%Y-%m-%d %H:%M:%S")
+    config['Time'] = time_str
+
+    try:
+        config['commit_id'] = get_git_commit_id()
+    except:
+        print("[WARNING] Couldn't get the git commit id")
+    
+    print(OmegaConf.to_yaml(config, resolve=True))
+    return config
+
+
+config = parse_arguments()
+
+
+## 1) Define the model
+model = XMFNetPL(config)
+
+## 2) Define the dataloader
+opt = config.dataset
+ViPCDataset_train = ViPCDataLoader(
+    'dataset/train_list2.txt', data_path=opt.dataroot, status="train", category=opt.cat)
+train_dataloader = DataLoader(ViPCDataset_train,
+                              collate_fn=collate_fn,
+                              batch_size=opt.batch_size,
+                              num_workers=opt.nThreads,
+                              shuffle=True,
+                              drop_last=True)
+ViPCDataset_test = ViPCDataLoader(
+    'dataset/test_list2.txt', data_path=opt.dataroot, status="test", category=opt.cat)
+val_dataloader = DataLoader(ViPCDataset_test,
+                            collate_fn=collate_fn,
+                            batch_size=opt.batch_size,
+                            num_workers=opt.nThreads,
+                            shuffle=True,
+                            drop_last=True)
+
+# trainer = pl.Trainer(devices=1, accelerator="gpu", **config.trainer)
+# trainer.fit(model, train_dataloader, val_dataloader)
+
